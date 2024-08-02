39111eeaf606581cc0e95632fc697588455da724
Haniya Mumtaj <93676451+han1159@users.noreply.github.com>
2024-08-02 21:32:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/bfhl', (req, res) => {
